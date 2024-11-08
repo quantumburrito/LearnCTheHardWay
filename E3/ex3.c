249eed14ccfd459e58cc0fbdb09dfb363a55e9f0
System administrator <bfitzge@siue.edu>
2024-11-08 17:52:40 -0600

    finished E7

--- E3/ex3.c
+++ E3/ex3.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+void crash() {
+	char *test = NULL;
+	int i = 0;
+	for(i = 0; i < 100000; i++) {
+		printf("%c", test[i]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	int age = 10;
 	int height = 72;
 
-	printf("I am %d years old. \n",age );
 	printf("I am %d inches tall. \n\a", height);
+	crash();
+	crash();	
 
 	return 0;
 }
